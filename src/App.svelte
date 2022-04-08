7f1f0c7de5447793ac7a6471106cfdec51e6f367
William Sjöström <william.sjostrom@skf.com>
2022-04-08 13:20:36 +0200

    Buff calculator done

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+
+<script>
+	import MainCalc from "./MainCalc.svelte";
+	import "carbon-components-svelte/css/g80.css";
+</script>
+
+<main>
+	<div id="title">
+		<h1>Williams Buff Percentage Calculator</h1>
+	</div>
+	
+	<div>
+		<MainCalc/>
+	</div>
+</main>
+
+<style>
+	#title {
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		display: flex;
+	}
+</style>
